b0981a6cc6fe0a91751da41add6f5bd289169540
Alvin Mwakai <mwakaimwambala@gmail.com>
2022-11-16 22:02:51 +0300

    init

--- app/src/main/java/com/example/maternalhealth/RegisterActivity.kt
+++ app/src/main/java/com/example/maternalhealth/RegisterActivity.kt
@@ -2,10 +2,14 @@ package com.example.maternalhealth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN)
         setContentView(R.layout.activity_register)
     }
 }
