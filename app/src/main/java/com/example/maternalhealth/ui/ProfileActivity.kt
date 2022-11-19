696b92ac8fd276bfd100fc309e3d3a4b60aa35e1
Alvin Mwakai <mwakaimwambala@gmail.com>
2022-11-19 14:11:53 +0300

    Profile

---
+++ app/src/main/java/com/example/maternalhealth/ui/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.example.maternalhealth.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.maternalhealth.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
