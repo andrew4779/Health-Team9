ccc686540e894336dcca6723b1d43ea7c4c026b2
Alvin Mwakai <mwakaimwambala@gmail.com>
2022-11-22 17:06:30 +0300

    init

--- app/src/main/java/com/example/maternalhealth/MainActivity.kt
+++ app/src/main/java/com/example/maternalhealth/MainActivity.kt
@@ -2,16 +2,19 @@ package com.example.maternalhealth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.MenuItem
 import android.view.WindowManager
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
-            WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN
-        )
         setContentView(R.layout.activity_main)
 
+
     }
+
 }
